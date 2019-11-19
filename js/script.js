eb1d144c477e4b3e22e8e80e141402d89373b359
akimestr <mkestrade@gmail.com>
2019-11-19 01:14:16 -0300

    Estrutura base(homepage)

---
+++ js/script.js
@@ -0,0 +1,54 @@
+/* EVENTO SCROLL -> MUDAR O MENU */
+
+
+$(window).bind('scroll', function () {
+      if ($(window).scrollTop() > 300) {
+        $(".menu_mluv_retratil").addClass('mostrar');
+      } else if ($(window).scrollTop() < 300) {
+        $(".menu_mluv_retratil").removeClass('mostrar');
+      }
+});
+
+// JSSOR SLIDER
+ $(document).ready(function ($) {
+
+            var jssor_1_options = {
+              $AutoPlay: 1,
+              $SlideDuration: 800,
+              $SlideEasing: $Jease$.$OutQuint,
+              $ArrowNavigatorOptions: {
+                $Class: $JssorArrowNavigator$
+              },
+              $BulletNavigatorOptions: {
+                $Class: $JssorBulletNavigator$
+              }
+            };
+
+            var jssor_1_slider = new $JssorSlider$("jssor_1", jssor_1_options);
+
+            /*#region responsive code begin*/
+
+            var MAX_WIDTH = 3000;
+
+            function ScaleSlider() {
+                var containerElement = jssor_1_slider.$Elmt.parentNode;
+                var containerWidth = containerElement.clientWidth;
+
+                if (containerWidth) {
+
+                    var expectedWidth = Math.min(MAX_WIDTH || containerWidth, containerWidth);
+
+                    jssor_1_slider.$ScaleWidth(expectedWidth);
+                }
+                else {
+                    window.setTimeout(ScaleSlider, 30);
+                }
+            }
+
+            ScaleSlider();
+
+            $(window).bind("load", ScaleSlider);
+            $(window).bind("resize", ScaleSlider);
+            $(window).bind("orientationchange", ScaleSlider);
+            /*#endregion responsive code end*/
+        });
